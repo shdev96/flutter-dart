5b029d5aeafb933fdc9135fd0691d5a111f1217a
shdev96 <seunghee.dev@gmail.com>
2024-06-17 21:56:35 +0900

    class

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,25 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amber,
-        body: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Colors.red, Colors.blue],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topRight)),
-          child: const Center(
-            child: Text("hello world",
-                style: TextStyle(color: Colors.white, fontSize: 18)),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+// class must start with capital letter
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.blue, Colors.black],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topRight)),
+        child: const Center(
+          child: Text("hello world",
+              style: TextStyle(color: Colors.white, fontSize: 21)),
+        ));
+  }
 }
