41e19e5277bc413cbb62972266ec5ab553ed9bc4
shdev96 <seunghee.dev@gmail.com>
2024-06-17 22:47:47 +0900

    var & const

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,6 +2,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dart/style_text.dart';
 
+// variable must start with lower case
+// Alignment? startAlignment;
+// var startAlignment = Alignment.bottomLeft;
+const startAlignment = Alignment.bottomLeft;
+
+const endAlignment = Alignment.topRight;
+
 // class must start with capital letter
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -12,8 +19,8 @@ class GradientContainer extends StatelessWidget {
         decoration: const BoxDecoration(
             gradient: LinearGradient(
                 colors: [Colors.blue, Colors.black],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topRight)),
+                begin: startAlignment,
+                end: endAlignment)),
         child: const Center(
           child: StyleText(),
         ));
